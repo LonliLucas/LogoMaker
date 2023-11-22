6bfb5476d24d381f8c2d9f3440d4e4a6e49f01f1
LonliLucas <lukej.gilless@gmail.com>
2023-11-22 13:00:11 -0700

    Added functions to create svg file with answers.

--- index.js
+++ index.js
@@ -1,41 +1,62 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const { error } = require("console");
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-const questions = [{
-    type: 'input',
-    message: 'Enter 3 characters for your logo: ',
-    name: 'characters'
-},
-{
-    type: 'input',
-    message: 'Enter the color you want the characters to be: ',
-    name: 'textColor'
-},
-{
-    type: 'list',
-    message: 'Which shape should the backround be?',
-    name: 'shape'
-},
-{
-    type: 'input',
-    message: 'Enter the color you want the shape to be: ',
-    name: 'shapeColor'
-},
-{
-    type: 'input',
-    message: 'What do you want to name your logo file? ',
-    name: 'title'
-}];
+const questions = [
+  {
+    type: "input",
+    message: "Enter 3 characters for your logo: ",
+    name: "characters",
+  },
+  {
+    type: "input",
+    message: "Enter the color you want the characters to be: ",
+    name: "textColor",
+  },
+  {
+    type: "list",
+    message: "Which shape should the backround be?",
+    name: "shape",
+    choices: ["Square", "Circle", "Triangle"],
+  },
+  {
+    type: "input",
+    message: "Enter the color you want the shape to be: ",
+    name: "shapeColor",
+  },
+  {
+    type: "input",
+    message: "What do you want to name your logo file? ",
+    name: "title",
+  },
+];
 
-function LogoInfo(characters, textColor, shape, shapeColor, title) {
-    this.characters = characters;
-    this.textColor = textColor;
-    this.shape = shape;
-    this.shapeColor = shapeColor;
-    this.title = title;
-    this.createLogo = function() {
-
+function writeToFile(fileName, data) {
+  fs.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(err);
+      return;
     }
+    console.log(`File ${fileName} has been created!`)
+  });
 }
 
-console.log("eat this idiot");
+function createLogo(answers) {
+  return (
+    `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">\n` +
+    `<circle cx="150" cy="100" r="80" fill="${answers.shapeColor}" />\n` +
+    `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.characters}</text>\n` +
+    `</svg>`
+  );
+}
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const content = createLogo(answers);
+    writeToFile(`./output/${answers.title}.svg`, content);
+  }).catch((error) => {
+    console.error('Error occured:', error);
+  })
+};
+
+init();
